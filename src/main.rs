c829466d97b178388b74debc7a7b4316334cfe51
sam atman <atmanistan@gmail.com>
2014-11-27 14:48:29 -0800

    getting how this type system works

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,19 @@ use xterm::XString::* ;
 mod xterm {
     use std::fmt;
 
-    struct EscString(String);
-    struct JuString(String);
-    struct TString(String);
-
-    pub enum XString { Esc(EscString), Jump(JuString), Text(TString) }
+    pub enum XString { Esc(String), Jump(String), Text(String) }
 
     impl fmt::Show for XString {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let mut s = String::new();
             let s = match self {
-                &XString::Esc(s)  => s.0,
-                &XString::Jump(s) => s.0,
-                &XString::Text(s) => s.0,
+                &XString::Esc(ref q)  => q,
+                &XString::Jump(ref q) => q,
+                &XString::Text(ref q) => q,
             };
            write!(f,"{}",s)
+        }
     }
-
-}
 
     pub struct XVec {
         v: Vec<XString>,
@@ -32,17 +28,17 @@ mod xterm {
 
     pub fn print_x ( xstr: XString ) -> () {
         match xstr {
-            XString::Esc(s)  => println!("{}", s.0),
-            XString::Jump(s) => println!("{}", s.0),
-            XString::Text(s) => println!("{}", s.0),
+            XString::Esc(s)  => println!("{}", s),
+            XString::Jump(s) => println!("{}", s),
+            XString::Text(s) => println!("{}", s),
         }
     }
 
     #[allow(dead_code)]
     pub enum Colors { Red, Blue, Green, Yellow, Magenta, Cyan, White, Default }
 
-    pub fn color_fg ( col: Colors ) -> EscString {
-        let s = match col {
+    pub fn color_fg ( col: Colors ) -> XString {
+        XString::Esc(match col {
             Colors::Red     => "\u001b[31m",
             Colors::Blue    => "\u001b[34m",
             Colors::Green   => "\u001b[32m",
@@ -51,13 +47,11 @@ mod xterm {
             Colors::Cyan    => "\u001b[36m",
             Colors::White   => "\u001b[37m",
             Colors::Default => "\u001b[0m",
-        };
-
-        EscString(s.to_string())
+        }.to_string())
     }
 
-    pub fn color_bg ( col: Colors ) -> EscString {
-        EscString(match col {
+    pub fn color_bg ( col: Colors ) -> String {
+        match col {
             Colors::Red     => "\u001b[41m",
             Colors::Blue    => "\u001b[44m",
             Colors::Green   => "\u001b[42m",
@@ -66,7 +60,7 @@ mod xterm {
             Colors::Cyan    => "\u001b[46m",
             Colors::White   => "\u001b[47m",
             Colors::Default => "\u001b[0m",
-        }.to_string())
+        }.to_string()
 
     }
 }
@@ -74,9 +68,9 @@ mod xterm {
 
 fn main() {
     println!("Hello, world!");
-    let col_string: XString  = Esc(color_fg(Colors::Green)) ;
+    let col_string: XString  = color_fg(Colors::Green) ;
     print!("{}",Esc(color_bg(Colors::Magenta))) ;
     print_x(col_string);
-    print!("Oye! {}",Esc(color_fg(Colors::Default))) ;
+    print!("Oye! {}",color_fg(Colors::Default)) ;
     println!("");
 }
